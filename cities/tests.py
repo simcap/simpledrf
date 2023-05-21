b56bc9a5185b6b30ebab01201accb4feb19efc31
Simon <scaplette@candy.com>
2023-05-21 21:52:55 +0200

    add mock test for third party service

--- cities/tests.py
+++ cities/tests.py
@@ -1,5 +1,9 @@
 from django.test import TestCase
+from unittest.mock import patch
+from rest_framework import status
+from rest_framework.test import APITestCase
 from cities.models import City
+from cities.services import LocalWeatherService
 
 class CitiesTestCase(TestCase):
     def setUp(self):
@@ -10,3 +14,11 @@ class CitiesTestCase(TestCase):
         cities = City.objects.all()
         self.assertTrue(len(cities) == 2)
 
+
+class CitiesWithTemperature(APITestCase):
+    def test_create_city_and_temperature(self):
+        with patch.object(LocalWeatherService, 'fetch_weather', return_value=28.4):
+            response = self.client.post('/cities/new', {'name': 'London', 'lon': 76, 'lat': 0 })
+            self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+            city = City.objects.filter(name = 'London').first()
+            self.assertEqual(city.temperature, 28.4)
